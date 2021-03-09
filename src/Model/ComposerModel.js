bb7d0802d8dd3cbe8ce64c1d6d8868892e855bfa
Alexander Burdiss <aburdiss@icloud.com>
2021-03-08 22:35:49 -0500

    Added Composer Model

---
+++ src/Model/ComposerModel.js
@@ -0,0 +1,397 @@
+export const composers = {
+  Bach: {
+    id: 0,
+    name: 'Johann Sebastian Bach',
+    ipa: 'joˈhan zeˈbastjan bɑx',
+    image: 2000,
+    country: 'Germany',
+    dates: '1685-1750',
+    bio:
+      'Johann Sebastian Bach was a German Baroque composer and musician. He is most well known for is keyboard works, and works for voice and orchestra. Bach was a master of counterpoint and harmonic organization and his influences in these fields continue to affect composers to this day.',
+  },
+
+  Beethoven: {
+    id: 1,
+    name: 'Ludwig Van Beethoven',
+    ipa: 'ˈlʊdvɪɡ væn ˈbeɪt(h}oʊvən',
+    image: 2001,
+    country: 'Germany',
+    dates: '1770-1827',
+    bio:
+      'Ludwig van Beethoven was a German composer and pianist. Beethoven began his compositional life firmly in the classical era, but was a key figure in the early romantic era. Beethoven grew to be mostly deaf in his lifetime, but never ceased composing. He was the first composer to incorporate trombones in a symphonic setting, and composed nine symphonies in his lifetime. He is considered to be one of the greatest composers of all time.',
+  },
+
+  Berlioz: {
+    id: 2,
+    name: 'Hector Berlioz',
+    ipa: 'ɛkˈtɔr bɛr liˌoʊz',
+    image: 2002,
+    country: 'France',
+    dates: '1803-1869',
+    bio:
+      'Hector Berlioz was a French Romantic composer, who gained his popularity through his unwillingness to accept traditional rules and formulas. He frequently employed extended techniques in many instruments, and developed his own forms and techniques for composition. He was internationally acclaimed as a conductor, and was also a prominant musical journalist.',
+  },
+
+  Bizet: {
+    id: 3,
+    name: 'Georges Bizet',
+    ipa: 'zhawrzh biˈzeɪ',
+    image: 2003,
+    country: 'France',
+    dates: '1838-1875',
+    bio:
+      'Georges Bizet was a prominant French composer of the Romantic Era. Best known for his operas and incidental music, Bizet had little success until his final work "Carmen".',
+  },
+
+  Borodin: {
+    id: 4,
+    name: 'Alexander Borodin',
+    ipa: 'ɐlʲɪkˈsandr bərɐˈdʲin',
+    image: 2004,
+    country: 'Russia',
+    dates: '1833-1887',
+    bio:
+      'Alexander Borodin was a Russian Chemist and Romantic composer, who was one of the major composers who advocated for Russian nationalist music. Borodin is best known for his symphonies, and his only opera "Prince Igor". He also made major contributions to the field of organic chemistry, and was a major proponent of education, founding a school of medicine for women in St. Petersburg.',
+  },
+
+  Brahms: {
+    id: 5,
+    name: 'Johannes Brahms',
+    ipa: 'joˈhanəs ˈbʁaːms',
+    image: 2005,
+    country: 'Germany',
+    dates: '1833-1897',
+    bio:
+      'Johannes Brahms was one of the most famous composers of all time. He is most famous for his four symphonies, but he wrote for many diverse ensembles. Brahms was a perfectionist in all that he did, and was known to occaisionally destroy some of his work to prevent it from being seen by the outside world.',
+  },
+
+  Bruckner: {
+    id: 6,
+    name: 'Anton Bruckner',
+    ipa: " 'antɔn ˈbʁʊknɐ",
+    image: 2006,
+    country: 'Austria',
+    dates: '1824-1896',
+    bio:
+      'Anton Bruckner was an Austrian organist, music theorist, and composer of the romantic era. He is best known for his symphonies and religious music. Bruckner was unlike other composers of his time, in that he was very a very humble man.',
+  },
+
+  Debussy: {
+    id: 7,
+    name: 'Claude Debussy',
+    ipa: 'klod dəbysi',
+    image: 2006,
+    country: 'France',
+    dates: '1862-1918',
+    bio:
+      'Debussy is known as the first impressionist composer, although he strongly rejected the term in his lifetime. He was born to a modest family with little cultural involvement, but was admitted to the Conservatoire de Paris for his musical ability when he was 10 years old. His experimental compositional style was rejected by the Conservatory, but he gained international fame nearly 30 years later with the release of his first opera, " Pelléas et Mélisande".',
+  },
+
+  Donizetti: {
+    id: 8,
+    name: 'Gaetano Donizetti',
+    ipa: 'ɡaeˈtaːno donidˈdzetti',
+    image: 2007,
+    country: 'Italy',
+    dates: '1797-1848',
+    bio:
+      'Donizetti was one of the leading composers of the Bel Canto opera style, along with Rossini and Bellini. Donizetti did not come from a musical background, but was taken up by the composer Simon Mayr at a young age and enrolled in a music school. Throughout the course of his life he composed almost 70 operas. He moved to Paris towards the end of his career to escape the censorship that he was always subject to in Naples.',
+  },
+
+  Dvorak: {
+    id: 9,
+    name: 'Antonín Dvořák',
+    ipa: 'ˈɑn tɔ nyin ˈdvɔr ʒɑk',
+    image: 2007,
+    country: 'Austrian Empire',
+    dates: '1841-1904',
+    bio:
+      'Dvořák was one of the first Czech composers to recieve worldwide recognition. Dvořák was an admirer of Richard Wagner, and took influence from him especially in his Operas. He frequently included folk music in his compositions, and his music often had nationalistic themes.',
+  },
+
+  Franck: {
+    id: 10,
+    name: 'César Franck',
+    ipa: 'seɪˈzar frɑŋk',
+    image: 2008,
+    country: 'United Kingdom of the Netherlands (Now Belgium}',
+    dates: '1822 - 1890',
+    bio:
+      'Franck studied music in Paris at a young age. After his studies, he returned to Belgium and gained a terrible reputation for his oratorio "Ruth", and moved to Paris where he became an organist and teacher. Later in life, he became the organist for the Basilica of Saint Clotilde, where he remained for the rest of his life, composing in his free time pieces that are now considered standard repetoire.',
+  },
+
+  Handel: {
+    id: 11,
+    name: 'George Frideric Handel',
+    ipa: 'ˈɡeːɔʁk ˈfʁiːdʁɪç ˈhɛndl̩',
+    image: 2009,
+    country: 'England',
+    dates: '1685-1759',
+    bio:
+      'Handel was one of the most influential composers of the Baroque era. He studied music in Halle, began his career in Hamburg, but made his fame and wrote the majority of his compositions while living in London, providing music for a variety of settings including the three commercial opera companies he started while residing there. Handel was nearly blind by the time of his death, and his funeral was celebrated with full state honors.',
+  },
+
+  Haydn: {
+    id: 12,
+    name: 'Franz Joseph Haydn',
+    ipa: 'ˈfʁants ˈjoːzɛf ˈhaɪdn̩',
+    image: 2009,
+    country: 'Austria',
+    dates: '1732-1809',
+    bio:
+      'Haydn was a classical composer who is commonly known as "The father of the Symphony". He spent most of his life as a court musician for the Esterházy family. He was isolated in this role, with little to no contact with the rest of the music community, and was forced to create his own style because of this. Haydn was a prolific composer, producing 104 numbered symphonies and an immense amount of chamber music.',
+  },
+
+  Holst: {
+    id: 13,
+    name: 'Gustav Holst',
+    ipa: 'ˈgʊs tɑv hoʊlst',
+    image: 2010,
+    country: 'England',
+    dates: '1874-1934',
+    bio:
+      'Holst was a popular English composer of the modern era. He aspired to become a concert pianist, but neuritis in his right arm prevented him. Against his father\'s wishes, he studied to become a composer. He was unable to support himself from his compositions alone, and started to play and teach the trombone professionally. He is most well known for his orchestral composition "The Planets".',
+  },
+
+  Liszt: {
+    id: 14,
+    name: 'Franz Liszt',
+    ipa: 'ˈfɛrɛnt͡s ˈlist',
+    image: 2008,
+    country: 'Hungary',
+    dates: '1811-1886',
+    bio:
+      'Franz Liszt is regarded as one of the greatest pianists to ever live. A well known and travelled musician in his time, he invented what we now know as the recital, and set the precedent for memorizing solo compositions. Liszt also pioneered new paths in musical form and harmony, inventing the Symphonic Poem and made radical innovations in harmony.',
+  },
+
+  Mahler: {
+    id: 15,
+    name: 'Gustav Mahler',
+    ipa: 'ˈgʊs tɑf ˈmɑ lər',
+    image: 2011,
+    country: 'Austrian Empire',
+    dates: '1860-1911',
+    bio:
+      'Mahler is one of the most popular composers of all time. In 2016, a BBC Music survey ranked three of his symphonies among the top ten symphonies of all time. His music continues to be among the top recorded and perfomed in all of classical music. During his day, he was also among one of the top conductors of his era, and was the first conductor of the New York Philharmonic Orchestra.',
+  },
+
+  Mendellsohn: {
+    id: 16,
+    name: 'Felix Mendelssohn',
+    ipa: "'fi lɪks 'mɛn dl sən",
+    image: 2010,
+    country: 'Germany',
+    dates: '1809-1947',
+    bio:
+      'Mendelssohn was born into a Jewish family, but was baptised Reformed Christian at the age of seven. He was known as a child prodigy, but his parents did not seek to capitalize on his talents. Well known as a composer, conductor, and organist, Mendelssohn helped to bring fame to the music of Bach.',
+  },
+
+  Mozart: {
+    id: 17,
+    name: 'Wolfgang Amadeus Mozart',
+    ipa: 'ˈwʊlf gæŋ ˌæm əˈdeɪ əs ˈmoʊt sɑrt',
+    image: 2012,
+    country: 'Germany',
+    dates: '1756-1791',
+    bio:
+      "Mozart gained popularity as a pianist and composer at an early age. He gained employment as a court musician at the Salzburg court, but quickly lost interest and began traveling looking for other employment. In Vienna, he found great fame, but worked his whole life trying to achieve financial stability: a goal he was never able to achieve. Mozart's death is also very mysterious and controvercial.",
+  },
+
+  Mussorgsky: {
+    id: 18,
+    name: 'Modest Mussorgsky',
+    ipa: 'moʊˈdɛst mʊˈsɔrg ski',
+    image: 2013,
+    country: 'Russia',
+    dates: '1839-1881',
+    bio:
+      'Mussorgsky was one of the most important composers in Russia during his lifetime. He frequently incorporated folk music, and discarded western traditions in favor of Russian nationalism. Many of his works are most famous through arrangements by other composers.',
+  },
+
+  Ravel: {
+    id: 19,
+    name: 'Maurice Ravel',
+    ipa: 'moʊˈris rəˈvɛl',
+    image: 2014,
+    country: 'France',
+    dates: '1875-1937',
+    bio:
+      'Ravel was an impressionist French composer. He was a careful, and slow composer, and composed much less compared to many other composers. He was also one of the first composers to realize the potential of recorded music, and was one of the first composers to record their music and marked it to a larger audience this way.',
+  },
+
+  Respighi: {
+    id: 20,
+    name: 'Ottorino Respighi',
+    ipa: 'ottoˈriːno reˈspiːɡi',
+    image: 2015,
+    country: 'Italy',
+    dates: '1879-1936',
+    bio:
+      'Respighi was an Italian violinist, composer, and musicologist. Hist interest in musicology led to the composition of his three most famous compositions: "Fountains of Rome", "Pines of Rome", and "Roman Festivals"',
+  },
+
+  RimskyKorsakov: {
+    id: 21,
+    name: 'Nikolai Rimsky-Korsakov',
+    ipa: 'nʲɪkəˈlaj ˈrʲimskʲɪj ˈkorsəkəf',
+    image: 2016,
+    country: 'Russia',
+    dates: '1844-1908',
+    bio:
+      'Nikolai Rimsky-Korsakov was one of the most famous Russian composers. He was a master orchestrator, publishing a textbook on the subject using musical examples solely from his own compositions, and his most famous compositions are his orchestral works. Rimsky-Korsakov also spent a large portion of his musical life in association with the Russian military.',
+  },
+
+  Rossini: {
+    id: 22,
+    name: 'Gioachino Rossini',
+    ipa: 'dʒɔ ɑkˈki nɔ roʊˈsi ni',
+    image: 2017,
+    country: 'Italy',
+    dates: '1792-1868',
+    bio:
+      'Rossini was an Italian composer from the romantic era. His 39 operas gained him great success early in life, but he unexplainably gave up music almost entirely for the last 40 years of his life.',
+  },
+
+  SaintSaens: {
+    id: 23,
+    name: 'Camille Saint-Saëns',
+    ipa: 'kamij sɛ̃ sɑ̃(s}',
+    image: 2018,
+    country: 'France',
+    dates: '1835-1921',
+    bio:
+      'Saint-Saëns was a French composer, conductor, organist, and pianist during the romantic era. Revered as a genius, he lived a mostly conventional carreer as a church organist. His most notable student was Gabriel Fauré.',
+  },
+
+  Schubert: {
+    id: 24,
+    name: 'Franz Schubert',
+    ipa: 'ˈfʁant͡s ˈʃuːbɐt',
+    image: 2019,
+    country: 'Austria',
+    dates: '1797-1828',
+    bio:
+      'Schubert was a prolific Austrian composer during the late Classical and early Romantic eras. During his short lifetime he composed over 1500 works. During his lifetime, he recieved little fame and success. It was only after his lifetime when musicians such as Schumann, Mendelssohn, Liszt, and Brahms discovered and praised his music that it became well known and widely recieved.',
+  },
+
+  Schumann: {
+    id: 25,
+    name: 'Robert Schumann',
+    ipa: 'ˈrɒb ərt ˈʃu mɑn',
+    image: 2020,
+    country: 'Germany',
+    dates: '1810-1856',
+    bio:
+      "Schumann was one of the most popular composers of the romantic era. He gave up the study of law to become a concert pianist, but his dreams were crushed by a hand injury. He subsequently pursued a career in composition, and married his teacher's daughter, Clara Wieck. Schumann mainly composed for piano, but later in live began to compose lieder, orchestral works, and one opera.",
+  },
+
+  Scriabin: {
+    id: 26,
+    name: 'Alexander Nikolayevich Scriabin',
+    ipa: 'ɐlʲɪˈksandr nʲɪkəˈɫaɪvʲɪtɕ ˈskrʲæbʲɪn',
+    image: 2016,
+    country: 'Russia',
+    dates: '1872-1915',
+    bio:
+      'Scriabin is known as the best Russian Symbolist composer. Much of his compositional output experiments with synesthesia, blending the perception of sound with the perception of color. His early works are notably tonal, taking cues from Frédéric Chopin. He later began experimenting with atonality independently of Arnold Schoenberg, incorporating various aspects of mysticism and synesthesia to create music he believed aligned with his personal brand of mysticism.',
+  },
+
+  Sibelius: {
+    id: 27,
+    name: 'Jean Sibelius',
+    ipa: 'ʒɑn  sɪˈbeɪ li əs',
+    image: 2017,
+    country: 'Finland',
+    dates: '1865-1957',
+    bio:
+      'Sibelius is the most well known composer to come out of Finland. His music is credited with helping Finland to develop a national identity, and his most famous work "Finlandia" is known as Finland\'s unofficial national anthem. Sibelius mysteriously stopped composing in the last thirty years of his life, retiring to a quite life in Järvenpää.',
+  },
+
+  Smetana: {
+    id: 28,
+    name: 'Bedřich Smetana',
+    ipa: 'ˌbɛdərʒɪx ˈsmɛtənə',
+    image: 2021,
+    country: 'Austrian Empire, Austria-Hugary (Now Czech Republic}',
+    dates: '1824-1884',
+    bio:
+      'Smetana was a child prodigy who had his first composition performed at age six. His music became closely tied to sentiments and movements of independant statehood. He became compely deaf for the last ten years of his life.',
+  },
+
+  Sousa: {
+    id: 29,
+    name: 'John Philip Sousa',
+    ipa: "dʒɒn ˈfɪl ɪp 'su zə",
+    image: 2012,
+    country: 'The United States of America',
+    dates: '1854–1932',
+    bio:
+      'John Philip Sousa was an American composer of the late romantic era famous for his American military marches. He is known as "The March King". Sousa spent much of his career conducting The United States Marine Band, before starting his own band in 1892. Sousa also aided in the creation of the Sousaphone, a large brass instrument similar to the Helicon and Tuba.',
+  },
+
+  Strauss: {
+    id: 30,
+    name: 'Richard Strauss',
+    ipa: 'ˈʁɪçaʁt ˈʃtʁaʊs',
+    image: 2022,
+    country: 'Germany',
+    dates: '1864-1949',
+    bio:
+      "Richard Strauss was one of the leading composers of the Romantic Era. He, along with Gustav Mahler, were considered successors of Richard Wagner. Although Strauss composed music for almost every classical genre, he is most famous for his tone poems. Strauss' compositional output began when he was just six years old, and continued almost eighty years later until his death.",
+  },
+
+  Stravinsky: {
+    id: 31,
+    name: 'Igor Stravinsky',
+    ipa: 'ˈi gɔr strəˈvɪn ski',
+    image: 2023,
+    country: 'Russia',
+    dates: '1882-1971',
+    bio:
+      "Igor Stravinsky was one of the most influential composers of the twentieth century. Stravinsky's music is very unique from that of other composers, and often contains russian folk melodies. Throughout his life, the kinds of compositions that he composed also changed dramatically, from symphonies, to chamber music, to bals.",
+  },
+
+  Tchaikovsky: {
+    id: 32,
+    name: 'Pyotr Ilyich Tchaikovsky',
+    ipa: 'ˈpyɔtr iˈlyitʃ tʃaɪˈkɒfski',
+    image: 2024,
+    country: 'Russia',
+    dates: '1840-1893',
+    bio:
+      "Tchaikovsky was the first Russian composer to gain international fame. Tchaikovsky recieved a western European style music education, which seemed to counter the traditional Russian music practices. This dichotomy posed a great challenge for Tchaikovsky, one that affected his self confidence a lot. Although Tchaikovsky's music gained popular support, his life was deeply affected by depression and personal crises.",
+  },
+
+  Verdi: {
+    id: 33,
+    name: 'Giuseppe Verdi',
+    ipa: 'dʒuˈzɛppe ˈverdi',
+    image: 2025,
+    country: 'Italy',
+    dates: '1813-1901',
+    bio:
+      'Giuseppe Verdi was an Italian opera composer who took after Rossini, Bellini, and Donizetti. Rossini had sympathy for the Risorgimento movement for the unification of Italy, and some choruses from his early operas were in the spirit of this movement. Verdi participated briefly as an elected politician, and sought to establish himself as a landowner in his native region.',
+  },
+
+  Wagner: {
+    id: 34,
+    name: 'Richard Wagner',
+    ipa: 'ˈʁɪçaʁt ˈvaːɡnɐ',
+    image: 2026,
+    country: 'Germany',
+    dates: '1813-1883',
+    bio:
+      'Wagner is one of the most well known composers of all time. His music frequently appears in television shows and movies. He was most well known in his era for his operas, which usually run about four hours. Wagner was unusual as an opera composer because he himself wrote the liberetto for the operas he composed. Wagner had his own opera house constructed, which still stands to this day.',
+  },
+
+  Weber: {
+    id: 35,
+    name: 'Carl Maria von Weber',
+    ipa: 'kɑrl məˈriə fən ˈvābər',
+    image: 2021,
+    country: 'Germany',
+    dates: '1786-1826',
+    bio:
+      'Carl Maria von Weber was a German composer, conductor, pianist, guitarist, and music critic. He is widely regarded as one of the first significant composers of the romantic school. His contributions to the field of Opera greatly influenced the development of Romantische Oper in Germany, and he was a major influence to Marschner, Meyerbeer and Wagner.',
+  },
+};
