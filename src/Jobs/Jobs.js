e396a47d469ff9a3e24e87a734ebd17cb0a6d7e4
Alexander Burdiss <aburdiss@icloud.com>
2021-03-19 01:13:44 -0400

    Began adding Job RSS information

--- src/Jobs/Jobs.js
+++ src/Jobs/Jobs.js
@@ -1,8 +1,51 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, ScrollView} from 'react-native';
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
 
+/**
+ * @todo Install React Query
+ */
 const Jobs = () => {
-  return <View />;
+  const musicalChairsHornAPI = 'https://www.musicalchairs.info/rss/en/horn';
+  const musicalChairsTrumpetAPI =
+    'https://www.musicalchairs.info/rss/en/trumpet';
+  const musicalChairsTromboneAPI =
+    'https://www.musicalchairs.info/rss/en/trombone';
+  const musicalChairsTubaAPI = 'https://www.musicalchairs.info/rss/en/tuba';
+
+  const [selectedInstrumentIndex, setSelectedInstrumentIndex] = useState(0);
+
+  async function fetchJobs() {
+    const dataToFetch = [
+      musicalChairsHornAPI,
+      musicalChairsTrumpetAPI,
+      musicalChairsTromboneAPI,
+      musicalChairsTubaAPI,
+    ][selectedInstrumentIndex];
+    const response = await fetch(dataToFetch);
+    console.log(await response.text());
+  }
+
+  return (
+    <View>
+      <View style={styles.segmentedControlWrapper}>
+        <SegmentedControl
+          values={['Horn', 'Trumpet', 'Trombone', 'Tuba']}
+          selectedIndex={selectedInstrumentIndex}
+          onChange={(event) => {
+            setSelectedInstrumentIndex(event.nativeEvent.selectedSegmentIndex);
+          }}
+        />
+      </View>
+      <ScrollView></ScrollView>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  segmentedControlWrapper: {
+    padding: 10,
+  },
+});
 
 export default Jobs;
