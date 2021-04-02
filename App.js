5620efcccbe17bd1934c5553d4655a1c0a410596
Alexander Burdiss <aburdiss@icloud.com>
2021-04-02 00:06:33 -0400

    Moved Navigation components from App.js to their own directoryMoved Navigation components from App.js to their own directoryMoved Navigation components from App.js to their own directoryMoved Navigation components from App.js to their own directoryMoved Navigation components from App.js to their own directoryMoved Navigation components from App.js to their own directoryMoved Navigation components from App.js to their own directoryMoved Navigation components from App.js to their own directoryMoved Navigation components from App.js to their own directory

--- App.js
+++ App.js
@@ -2,194 +2,20 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createStackNavigator} from '@react-navigation/stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {useDarkMode} from 'react-native-dynamic';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import HeaderButton from './src/Components/HeaderButton';
 import {colors} from './src/Model/Model';
-import Composers from './src/Composers/Composers';
-import ComposerDetail from './src/Composers/ComposerDetail';
-import Excerpts from './src/Excerpts/Excerpts';
-import ExcerptDetail from './src/Excerpts/ExcerptDetail';
-import RandomExcerpt from './src/Excerpts/RandomExcerpt';
-import Jobs from './src/Jobs/Jobs';
-import More from './src/More/More';
-import TopExcerpts from './src/Jobs/TopExcerpts';
-import Licenses from './src/More/Licenses';
-import Acknowledgements from './src/More/Acknowledgements';
 
-import {PreferencesContext, PreferencesProvider} from './src/Model/Preferences';
-import PastJobs from './src/Jobs/PastJobs';
-import JobDetail from './src/Jobs/JobDetail';
+import {PreferencesProvider} from './src/Model/Preferences';
+
+import ExcerptsStack from './src/Navigation/ExcerptsStack';
+import ComposersStack from './src/Navigation/ComposersStack';
+import JobsStack from './src/Navigation/JobsStack';
+import MoreStack from './src/Navigation/MoreStack';
 
 const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-
-const ExcerptsStack = ({navigation}) => {
-  const DARKMODE = useDarkMode();
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerTintColor: DARKMODE ? colors.greenDark : colors.greenLight,
-        headerTitleStyle: {
-          color: DARKMODE ? colors.white : colors.black,
-        },
-        headerStyle: {
-          backgroundColor: DARKMODE ? colors.systemGray6Dark : colors.white,
-          borderBottomWidth: 1,
-          borderBottomColor: DARKMODE
-            ? colors.systemGray5Dark
-            : colors.systemGray5Light,
-          shadowColor: 'transparent',
-        },
-        headerBackTitle: 'Back',
-      }}>
-      <Stack.Screen
-        name="Excerpts"
-        component={Excerpts}
-        options={{
-          headerRight: () => (
-            <HeaderButton
-              handler={() => {
-                navigation.navigate('Random Excerpt');
-              }}>
-              Random
-            </HeaderButton>
-          ),
-          title: 'BrassXcerpts',
-        }}
-      />
-      <Stack.Screen
-        name="Excerpt Detail"
-        component={ExcerptDetail}
-        options={({route}) => ({
-          title: route.params.name,
-        })}
-      />
-      <Stack.Screen name="Random Excerpt" component={RandomExcerpt} />
-    </Stack.Navigator>
-  );
-};
-
-const ComposersStack = ({navigation}) => {
-  const DARKMODE = useDarkMode();
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerTintColor: DARKMODE ? colors.greenDark : colors.greenLight,
-        headerTitleStyle: {
-          color: DARKMODE ? colors.white : colors.black,
-        },
-        headerStyle: {
-          backgroundColor: DARKMODE ? colors.systemGray6Dark : colors.white,
-          borderBottomWidth: 1,
-          borderBottomColor: DARKMODE
-            ? colors.systemGray5Dark
-            : colors.systemGray5Light,
-          shadowColor: 'transparent',
-        },
-        headerBackTitle: 'Back',
-      }}>
-      <Stack.Screen name="Composers" component={Composers} />
-      <Stack.Screen
-        name="Composer Detail"
-        component={ComposerDetail}
-        options={({route}) => ({
-          title: route.params.name,
-        })}
-      />
-      <Stack.Screen
-        name="Composer Excerpt Detail"
-        component={ExcerptDetail}
-        options={({route}) => ({
-          title: route.params.name,
-        })}
-      />
-    </Stack.Navigator>
-  );
-};
-
-const JobsStack = ({navigation}) => {
-  const DARKMODE = useDarkMode();
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerTintColor: DARKMODE ? colors.greenDark : colors.greenLight,
-        headerTitleStyle: {
-          color: DARKMODE ? colors.white : colors.black,
-        },
-        headerStyle: {
-          backgroundColor: DARKMODE ? colors.systemGray6Dark : colors.white,
-          borderBottomWidth: 1,
-          borderBottomColor: DARKMODE
-            ? colors.systemGray5Dark
-            : colors.systemGray5Light,
-          shadowColor: 'transparent',
-        },
-        headerBackTitle: 'Back',
-      }}>
-      <Stack.Screen
-        name="Jobs"
-        component={Jobs}
-        options={{
-          headerRight: () => (
-            <HeaderButton
-              handler={() => {
-                navigation.navigate('Past Auditions');
-              }}>
-              Past
-            </HeaderButton>
-          ),
-          title: 'Current Auditions',
-        }}
-      />
-      <Stack.Screen name="Top Excerpts" component={TopExcerpts} />
-      <Stack.Screen
-        name="Job Detail"
-        component={JobDetail}
-        options={({route}) => ({
-          title: route.params.orchestra,
-        })}
-      />
-      <Stack.Screen
-        name="Jobs Excerpt Detail"
-        component={ExcerptDetail}
-        options={({route}) => ({
-          title: route.params.name,
-        })}
-      />
-      <Stack.Screen name="Past Auditions" component={PastJobs} />
-    </Stack.Navigator>
-  );
-};
-
-const MoreStack = ({navigation}) => {
-  const DARKMODE = useDarkMode();
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerTintColor: DARKMODE ? colors.greenDark : colors.greenLight,
-        headerTitleStyle: {
-          color: DARKMODE ? colors.white : colors.black,
-        },
-        headerStyle: {
-          backgroundColor: DARKMODE ? colors.systemGray6Dark : colors.white,
-          borderBottomWidth: 1,
-          borderBottomColor: DARKMODE
-            ? colors.systemGray5Dark
-            : colors.systemGray5Light,
-          shadowColor: 'transparent',
-        },
-        headerBackTitle: 'Back',
-      }}>
-      <Stack.Screen name="More" component={More} />
-      <Stack.Screen name="Licenses" component={Licenses} />
-      <Stack.Screen name="Acknowledgements" component={Acknowledgements} />
-    </Stack.Navigator>
-  );
-};
 
 const App = () => {
   const DARKMODE = useDarkMode();
---
+++ src/Navigation/MoreStack.js
@@ -0,0 +1,39 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {useDarkMode} from 'react-native-dynamic';
+
+import {colors} from '../Model/Model';
+import More from '../More/More';
+import Licenses from '../More/Licenses';
+import Acknowledgements from '../More/Acknowledgements';
+
+const Stack = createStackNavigator();
+
+const MoreStack = ({navigation}) => {
+  const DARKMODE = useDarkMode();
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: DARKMODE ? colors.greenDark : colors.greenLight,
+        headerTitleStyle: {
+          color: DARKMODE ? colors.white : colors.black,
+        },
+        headerStyle: {
+          backgroundColor: DARKMODE ? colors.systemGray6Dark : colors.white,
+          borderBottomWidth: 1,
+          borderBottomColor: DARKMODE
+            ? colors.systemGray5Dark
+            : colors.systemGray5Light,
+          shadowColor: 'transparent',
+        },
+        headerBackTitle: 'Back',
+      }}>
+      <Stack.Screen name="More" component={More} />
+      <Stack.Screen name="Licenses" component={Licenses} />
+      <Stack.Screen name="Acknowledgements" component={Acknowledgements} />
+    </Stack.Navigator>
+  );
+};
+
+export default MoreStack;
