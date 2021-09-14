1c70e4d7bdb9eede1e281ba25605c04be0d8a375
Alexander Burdiss <aburdiss@icloud.com>
2021-09-14 00:20:11 -0400

    Add imslp link bach through beethoven

--- src/Model/AllExcerptDataModel/AllExcerptDataModel.js
+++ src/Model/AllExcerptDataModel/AllExcerptDataModel.js
@@ -1,22 +1,75 @@
 const AllExcerptDataModel = {
-  bachBrandenburg1: {},
-  bachBrandenburg2: {},
-  bachCantata51: {},
-  bachChristmasOratorio: {},
-  bachCelloSuite: {},
-  bachMagnificant: {},
-  bachMassB: {},
-  beethovenFidelio: {},
-  beethovenLeonore2: {},
-  beethovenLeonore3: {},
-  beethoven2: {},
-  beethoven3: {},
-  beethoven5: {},
-  beethoven6: {},
-  beethoven7: {},
-  beethoven8: {},
-  beethoven9: {},
-  beethovenViolin: {},
+  bachBrandenburg1: {
+    imslp:
+      'https://imslp.org/wiki/Brandenburg_Concerto_No.1_in_F_major%2C_BWV_1046_(Bach%2C_Johann_Sebastian)',
+  },
+  bachBrandenburg2: {
+    imslp:
+      'https://imslp.org/wiki/Brandenburg_Concerto_No.2_in_F_major,_BWV_1047_(Bach,_Johann_Sebastian)',
+  },
+  bachCantata51: {
+    imslp:
+      'https://imslp.org/wiki/Jauchzet_Gott_in_allen_Landen,_BWV_51_(Bach,_Johann_Sebastian)',
+  },
+  bachChristmasOratorio: {
+    imslp:
+      'https://imslp.org/wiki/Weihnachtsoratorium,_BWV_248_(Bach,_Johann_Sebastian)',
+  },
+  bachCelloSuite: {
+    imslp:
+      'https://imslp.org/wiki/Cello_Suite_No.5_in_C_minor,_BWV_1011_(Bach,_Johann_Sebastian)',
+  },
+  bachMagnificant: {
+    imslp:
+      'https://imslp.org/wiki/Magnificat_in_D_major%2C_BWV_243_(Bach%2C_Johann_Sebastian)',
+  },
+  bachMassB: {
+    imslp:
+      'https://imslp.org/wiki/Mass_in_B_minor%2C_BWV_232_(Bach%2C_Johann_Sebastian)',
+  },
+  beethovenFidelio: {
+    imslp: 'https://imslp.org/wiki/Fidelio,_Op.72_(Beethoven,_Ludwig_van)',
+  },
+  beethovenLeonore2: {
+    imslp:
+      'https://imslp.org/wiki/Leonora_Overture_No.2%2C_Op.72a_(Beethoven%2C_Ludwig_van)',
+  },
+  beethovenLeonore3: {
+    imslp:
+      'https://imslp.org/wiki/Leonora_Overture_No.3%2C_Op.72b_(Beethoven%2C_Ludwig_van)',
+  },
+  beethoven2: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.2,_Op.36_(Beethoven,_Ludwig_van)',
+  },
+  beethoven3: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.3,_Op.55_(Beethoven,_Ludwig_van)',
+  },
+  beethoven5: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.5,_Op.67_(Beethoven,_Ludwig_van)',
+  },
+  beethoven6: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.6,_Op.68_(Beethoven,_Ludwig_van)',
+  },
+  beethoven7: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.7,_Op.92_(Beethoven,_Ludwig_van)',
+  },
+  beethoven8: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.8,_Op.93_(Beethoven,_Ludwig_van)',
+  },
+  beethoven9: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.9,_Op.125_(Beethoven,_Ludwig_van)',
+  },
+  beethovenViolin: {
+    imslp:
+      'https://imslp.org/wiki/Violin_Concerto_in_D_major,_Op.61_(Beethoven,_Ludwig_van)',
+  },
   berliozFaust: {},
   berliozBenvenuto: {},
   berliozKingLear: {},
