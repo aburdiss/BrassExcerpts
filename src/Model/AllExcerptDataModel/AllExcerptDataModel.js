c4e04c3dced0a00186d3d760ae9ba10826bfefd2
Alexander Burdiss <aburdiss@icloud.com>
2021-09-16 23:47:09 -0400

    Add all imslp links to AllExcerptDataModel.js

--- src/Model/AllExcerptDataModel/AllExcerptDataModel.js
+++ src/Model/AllExcerptDataModel/AllExcerptDataModel.js
@@ -70,123 +70,419 @@ const AllExcerptDataModel = {
     imslp:
       'https://imslp.org/wiki/Violin_Concerto_in_D_major,_Op.61_(Beethoven,_Ludwig_van)',
   },
-  berliozFaust: {},
-  berliozBenvenuto: {},
-  berliozKingLear: {},
-  berliozLeCorsaire: {},
-  berliozRomanCarnival: {},
-  berliozRomeoJuliet: {},
-  berliozSymphonie: {},
-  bizetCarmen: {},
-  bizetLArlesienne: {},
-  borodinPolovetsian: {},
-  brahmsAcademic: {},
-  brahmsPiano1: {},
-  brahmsPiano2: {},
-  brahms1: {},
-  brahms2: {},
-  brahms3: {},
-  brahms4: {},
-  brahmsTragic: {},
-  brahmsVariations: {},
-  bruckner4: {},
-  bruckner7: {},
-  bruckner8: {},
-  bruckner9: {},
-  debussyNocturnes: {},
-  debussyLaMer: {},
-  donizettiDonPasquale: {},
-  dvorakCello: {},
-  dvorak8: {},
-  dvorak9: {},
-  franckD: {},
-  handelMessiah: {},
-  haydnCreation: {},
-  haydn31: {},
-  haydn100: {},
-  holstFirstSuite: {},
-  holstPlanets: {},
-  lisztLesPreludes: {},
-  mahlerDasLied: {},
-  mahler1: {},
-  mahler2: {},
-  mahler3: {},
-  mahler4: {},
-  mahler5: {},
-  mahler6: {},
-  mahler7: {},
-  mahler9: {},
-  mendelssohnMidsummer: {},
-  mendelssohn3: {},
-  mozartRequiem: {},
-  mozartSerenade9: {},
-  mozart29: {},
-  mozart40: {},
-  mussorgskyNightBald: {},
-  mussorgskyPictures: {},
-  ravelBolero: {},
-  respighiFountains: {},
-  rimskyKorsakovCapriccio: {},
-  rimskyKorsakovGolden: {},
-  rimskyKorsakovRussianEaster: {},
-  rimskyKorsakovScheherazade: {},
-  rossiniLaGazza: {},
-  rossiniSemiramide: {},
-  rossiniWilliamTell: {},
-  saintSaens3: {},
-  schubert8: {},
-  schubert9: {},
-  schumann1: {},
-  schumann2: {},
-  schumann3: {},
-  schumann4: {},
-  scriabin4: {},
-  sibelius1: {},
-  sibelius2: {},
-  smetanaBarteredBride: {},
-  sousaStarsAndStripes: {},
-  straussAlpine: {},
-  straussZarathustra: {},
-  straussBorgeois: {},
-  straussDeathAndTransfiguration: {},
-  straussDonJuan: {},
-  straussDonQuixote: {},
-  straussHeldenleben: {},
-  straussSalome: {},
-  straussSinfoniaDomestica: {},
-  straussEulenspiegel: {},
-  stravinskySoldier: {},
-  stravinskyFireworks: {},
-  stravinskyPetrushka: {},
-  stravinskyRite: {},
-  stravinskyNightengale: {},
-  stravinskyFirebird: {},
-  tchaikovskyRomeoAndJuliet: {},
-  tchaikovskyCapriccio: {},
-  tchaikovsky1: {},
-  tchaikovsky2: {},
-  tchaikovsky4: {},
-  tchaikovsky5: {},
-  tchaikovsky6: {},
-  tchaikovskyFrancesca: {},
-  tchaikovskyNutcracker: {},
-  tchaikovskySleepingBeauty: {},
-  tchaikovskySwanLake: {},
-  verdiForza: {},
-  verdiNabucco: {},
-  wagnerRheingold: {},
-  wagnerFaust: {},
-  wagnerFlying: {},
-  wagnerValkyrie: {},
-  wagnerGotterdammerung: {},
-  wagnerLohengrin: {},
-  wagnerMeistersinger: {},
-  wagnerParsifal: {},
-  wagnerRienzi: {},
-  wagnerSigfried: {},
-  wagnerTannhauser: {},
-  weberFrieschutz: {},
-  weberOberon: {},
+  berliozFaust: {
+    imslp:
+      'https://imslp.org/wiki/La_damnation_de_Faust%2C_H_111_(Berlioz%2C_Hector)',
+  },
+  berliozBenvenuto: {
+    imslp: 'https://imslp.org/wiki/Benvenuto_Cellini,_H_76_(Berlioz,_Hector)',
+  },
+  berliozKingLear: {
+    imslp:
+      'https://imslp.org/wiki/Overture_%27King_Lear%27%2C_H_53_(Berlioz%2C_Hector)',
+  },
+  berliozLeCorsaire: {
+    imslp:
+      'https://imslp.org/wiki/Overture_%27Le_Corsaire%27%2C_H_101_(Berlioz%2C_Hector)',
+  },
+  berliozRomanCarnival: {
+    imslp:
+      'https://imslp.org/wiki/Le_Carnaval_romain%2C_H_95_(Berlioz%2C_Hector)',
+  },
+  berliozRomeoJuliet: {
+    imslp:
+      'https://imslp.org/wiki/Rom%C3%A9o_et_Juliette%2C_H_79_(Berlioz%2C_Hector)',
+  },
+  berliozSymphonie: {
+    imslp:
+      'https://imslp.org/wiki/Symphonie_fantastique,_H_48_(Berlioz,_Hector)',
+  },
+  bizetCarmen: {
+    imslp: 'https://imslp.org/wiki/Carmen_(Bizet%2C_Georges)',
+  },
+  bizetLArlesienne: {
+    imslp:
+      'https://imslp.org/wiki/L%27Arl%C3%A9sienne_Suite_No.2_(Bizet%2C_Georges)',
+  },
+  borodinPolovetsian: {
+    imslp: 'https://imslp.org/wiki/Prince_Igor_(Borodin%2C_Aleksandr)',
+  },
+  brahmsAcademic: {
+    imslp:
+      'https://imslp.org/wiki/Academic_Festival_Overture%2C_Op.80_(Brahms%2C_Johannes)',
+  },
+  brahmsPiano1: {
+    imslp:
+      'https://imslp.org/wiki/Piano_Concerto_No.1,_Op.15_(Brahms,_Johannes)',
+  },
+  brahmsPiano2: {
+    imslp:
+      'https://imslp.org/wiki/Piano_Concerto_No.2%2C_Op.83_(Brahms%2C_Johannes)',
+  },
+  brahms1: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.1%2C_Op.68_(Brahms%2C_Johannes)',
+  },
+  brahms2: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.2%2C_Op.73_(Brahms%2C_Johannes)',
+  },
+  brahms3: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.3%2C_Op.90_(Brahms%2C_Johannes)',
+  },
+  brahms4: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.4%2C_Op.98_(Brahms%2C_Johannes)',
+  },
+  brahmsTragic: {
+    imslp: 'https://imslp.org/wiki/Tragic_Overture,_Op.81_(Brahms,_Johannes)',
+  },
+  brahmsVariations: {
+    imslp:
+      'https://imslp.org/wiki/Variations_on_a_Theme_by_Haydn%2C_Op.56_(Brahms%2C_Johannes)',
+  },
+  bruckner4: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.4_in_E-flat_major,_WAB_104_(Bruckner,_Anton)',
+  },
+  bruckner7: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.7_in_E_major%2C_WAB_107_(Bruckner%2C_Anton)',
+  },
+  bruckner8: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.8_in_C_minor%2C_WAB_108_(Bruckner%2C_Anton)',
+  },
+  bruckner9: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.9_in_D_minor%2C_WAB_109%2F143_(Bruckner%2C_Anton)',
+  },
+  debussyNocturnes: {
+    imslp: 'https://imslp.org/wiki/Nocturnes_(Debussy%2C_Claude)',
+  },
+  debussyLaMer: {
+    imslp: 'https://imslp.org/wiki/La_mer_(Debussy%2C_Claude)',
+  },
+  donizettiDonPasquale: {
+    imslp: 'https://imslp.org/wiki/Don_Pasquale_(Donizetti%2C_Gaetano)',
+  },
+  dvorakCello: {
+    imslp:
+      'https://imslp.org/wiki/Cello_Concerto%2C_Op.104_(Dvo%C5%99%C3%A1k%2C_Anton%C3%ADn)',
+  },
+  dvorak8: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.8%2C_Op.88_(Dvo%C5%99%C3%A1k%2C_Anton%C3%ADn)',
+  },
+  dvorak9: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.9,_Op.95_(Dvo%C5%99%C3%A1k,_Anton%C3%ADn)',
+  },
+  franckD: {
+    imslp: 'https://imslp.org/wiki/Symphony_in_D_minor_(Franck%2C_C%C3%A9sar)',
+  },
+  handelMessiah: {
+    imslp: 'https://imslp.org/wiki/Messiah,_HWV_56_(Handel,_George_Frideric)',
+  },
+  haydnCreation: {
+    imslp:
+      'https://imslp.org/wiki/Die_Sch%C3%B6pfung%2C_Hob.XXI:2_(Haydn%2C_Joseph)',
+  },
+  haydn31: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.31_in_D_major,_Hob.I:31_(Haydn,_Joseph)',
+  },
+  haydn100: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.100_in_G_major%2C_Hob.I:100_(Haydn%2C_Joseph)',
+  },
+  holstFirstSuite: {
+    imslp:
+      'https://imslp.org/wiki/First_Suite_for_Military_Band%2C_Op.28_No.1_(Holst%2C_Gustav)',
+  },
+  holstPlanets: {
+    imslp: 'https://imslp.org/wiki/The_Planets,_Op.32_(Holst,_Gustav)',
+  },
+  lisztLesPreludes: {
+    imslp: 'https://imslp.org/wiki/Les_pr%C3%A9ludes%2C_S.97_(Liszt%2C_Franz)',
+  },
+  mahlerDasLied: {
+    imslp: 'https://imslp.org/wiki/Das_Lied_von_der_Erde_(Mahler%2C_Gustav)',
+  },
+  mahler1: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.1_(Mahler,_Gustav)',
+  },
+  mahler2: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.2_(Mahler%2C_Gustav)',
+  },
+  mahler3: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.3_(Mahler%2C_Gustav)',
+  },
+  mahler4: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.4_(Mahler%2C_Gustav)',
+  },
+  mahler5: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.5_(Mahler%2C_Gustav)',
+  },
+  mahler6: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.6_(Mahler%2C_Gustav)',
+  },
+  mahler7: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.7_(Mahler%2C_Gustav)',
+  },
+  mahler9: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.9_(Mahler%2C_Gustav)',
+  },
+  mendelssohnMidsummer: {
+    imslp:
+      'https://imslp.org/wiki/A_Midsummer_Night%27s_Dream,_incidental_music,_Op.61_(Mendelssohn,_Felix)',
+  },
+  mendelssohn3: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.3%2C_Op.56_(Mendelssohn%2C_Felix)',
+  },
+  mozartRequiem: {
+    imslp:
+      'https://imslp.org/wiki/Requiem_in_D_minor%2C_K.626_(Mozart%2C_Wolfgang_Amadeus)',
+  },
+  mozartSerenade9: {
+    imslp:
+      'https://imslp.org/wiki/Serenade_in_D_major,_K.320_(Mozart,_Wolfgang_Amadeus)',
+  },
+  mozart29: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.29_in_A_major%2C_K.201%2F186a_(Mozart%2C_Wolfgang_Amadeus)',
+  },
+  mozart40: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.40_in_G_minor,_K.550_(Mozart,_Wolfgang_Amadeus)',
+  },
+  mussorgskyNightBald: {
+    imslp:
+      'https://imslp.org/wiki/Night_on_Bald_Mountain_(Mussorgsky%2C_Modest)',
+  },
+  mussorgskyPictures: {
+    imslp:
+      'https://imslp.org/wiki/Pictures_at_an_Exhibition_(Mussorgsky%2C_Modest)',
+  },
+  ravelBolero: {
+    imslp: 'https://imslp.org/wiki/Bol%C3%A9ro_(Ravel%2C_Maurice)',
+  },
+  respighiFountains: {
+    imslp: 'https://imslp.org/wiki/Fontane_di_Roma_(Respighi%2C_Ottorino)',
+  },
+  rimskyKorsakovCapriccio: {
+    imslp:
+      'https://imslp.org/wiki/Spanish_Capriccio%2C_Op.34_(Rimsky-Korsakov%2C_Nikolay)',
+  },
+  rimskyKorsakovGolden: {
+    imslp:
+      'https://imslp.org/wiki/The_Golden_Cockerel_(suite)_(Rimsky-Korsakov%2C_Nikolay)',
+  },
+  rimskyKorsakovRussianEaster: {
+    imslp:
+      'https://imslp.org/wiki/Russian_Easter_Festival%2C_Op.36_(Rimsky-Korsakov%2C_Nikolay)',
+  },
+  rimskyKorsakovScheherazade: {
+    imslp:
+      'https://imslp.org/wiki/Scheherazade,_Op.35_(Rimsky-Korsakov,_Nikolay)',
+  },
+  rossiniLaGazza: {
+    imslp: 'https://imslp.org/wiki/La_gazza_ladra_(Rossini%2C_Gioacchino)',
+  },
+  rossiniSemiramide: {
+    imslp: 'https://imslp.org/wiki/Semiramide_(Rossini%2C_Gioacchino)',
+  },
+  rossiniWilliamTell: {
+    imslp: 'https://imslp.org/wiki/Guillaume_Tell_(Rossini%2C_Gioacchino)',
+  },
+  saintSaens3: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.3%2C_Op.78_(Saint-Sa%C3%ABns%2C_Camille)',
+  },
+  schubert8: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.8,_D.759_(Schubert,_Franz)',
+  },
+  schubert9: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.9%2C_D.944_(Schubert%2C_Franz)',
+  },
+  schumann1: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.1%2C_Op.38_(Schumann%2C_Robert)',
+  },
+  schumann2: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.2%2C_Op.61_(Schumann%2C_Robert)',
+  },
+  schumann3: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.3%2C_Op.97_(Schumann%2C_Robert)',
+  },
+  schumann4: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.4%2C_Op.120_(Schumann%2C_Robert)',
+  },
+  scriabin4: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.4%2C_Op.54_(Scriabin%2C_Aleksandr)',
+  },
+  sibelius1: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.1,_Op.39_(Sibelius,_Jean)',
+  },
+  sibelius2: {
+    imslp: 'https://imslp.org/wiki/Symphony_No.2,_Op.43_(Sibelius,_Jean)',
+  },
+  smetanaBarteredBride: {
+    imslp:
+      'https://imslp.org/wiki/The_Bartered_Bride%2C_JB_1:100_(Smetana%2C_Bed%C5%99ich)',
+  },
+  sousaStarsAndStripes: {
+    imslp:
+      'https://imslp.org/wiki/The_Stars_and_Stripes_Forever_(Sousa,_John_Philip)',
+  },
+  straussAlpine: {
+    imslp:
+      'https://imslp.org/wiki/Eine_Alpensinfonie,_Op.64_(Strauss,_Richard)',
+  },
+  straussZarathustra: {
+    imslp:
+      'https://imslp.org/wiki/Also_sprach_Zarathustra,_Op.30_(Strauss,_Richard)',
+  },
+  straussBorgeois: {
+    imslp:
+      'https://imslp.org/wiki/Der_B%C3%BCrger_als_Edelmann_Suite%2C_TrV_228c_(Strauss%2C_Richard)',
+  },
+  straussDeathAndTransfiguration: {
+    imslp:
+      'https://imslp.org/wiki/Tod_und_Verkl%C3%A4rung,_Op.24_(Strauss,_Richard)',
+  },
+  straussDonJuan: {
+    imslp: 'https://imslp.org/wiki/Don_Juan,_Op.20_(Strauss,_Richard)',
+  },
+  straussDonQuixote: {
+    imslp: 'https://imslp.org/wiki/Don_Quixote,_Op.35_(Strauss,_Richard)',
+  },
+  straussHeldenleben: {
+    imslp: 'https://imslp.org/wiki/Ein_Heldenleben,_Op.40_(Strauss,_Richard)',
+  },
+  straussSalome: {
+    imslp: 'https://imslp.org/wiki/Salome,_Op.54_(Strauss,_Richard)',
+  },
+  straussSinfoniaDomestica: {
+    imslp:
+      'https://imslp.org/wiki/Sinfonia_Domestica,_Op.53_(Strauss,_Richard)',
+  },
+  straussEulenspiegel: {
+    imslp:
+      'https://imslp.org/wiki/Till_Eulenspiegels_lustige_Streiche,_Op.28_(Strauss,_Richard)',
+  },
+  stravinskySoldier: {
+    imslp: 'https://imslp.org/wiki/L%27histoire_du_soldat_(Stravinsky%2C_Igor)',
+  },
+  stravinskyFireworks: {
+    imslp:
+      'https://imslp.org/wiki/Feu_d%27artifice%2C_Op.4_(Stravinsky%2C_Igor)',
+  },
+  stravinskyPetrushka: {
+    imslp: 'https://imslp.org/wiki/Petrushka_(Stravinsky%2C_Igor)',
+  },
+  stravinskyRite: {
+    imslp: 'https://imslp.org/wiki/The_Rite_of_Spring_(Stravinsky,_Igor)',
+  },
+  stravinskyNightengale: {
+    imslp: 'https://imslp.org/wiki/Le_Chant_du_rossignol_(Stravinsky%2C_Igor)',
+  },
+  stravinskyFirebird: {
+    imslp: 'https://imslp.org/wiki/The_Firebird_(Stravinsky,_Igor)',
+  },
+  tchaikovskyRomeoAndJuliet: {
+    imslp:
+      'https://imslp.org/wiki/Romeo_and_Juliet_(overture-fantasia)_(Tchaikovsky%2C_Pyotr)',
+  },
+  tchaikovskyCapriccio: {
+    imslp:
+      'https://imslp.org/wiki/Italian_Capriccio%2C_Op.45_(Tchaikovsky%2C_Pyotr)',
+  },
+  tchaikovsky1: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.1%2C_Op.13_(Tchaikovsky%2C_Pyotr)',
+  },
+  tchaikovsky2: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.2%2C_Op.17_(Tchaikovsky%2C_Pyotr)',
+  },
+  tchaikovsky4: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.4%2C_Op.36_(Tchaikovsky%2C_Pyotr)',
+  },
+  tchaikovsky5: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.5%2C_Op.64_(Tchaikovsky%2C_Pyotr)',
+  },
+  tchaikovsky6: {
+    imslp:
+      'https://imslp.org/wiki/Symphony_No.6%2C_Op.74_(Tchaikovsky%2C_Pyotr)',
+  },
+  tchaikovskyFrancesca: {
+    imslp:
+      'https://imslp.org/wiki/Francesca_da_Rimini%2C_Op.32_(Tchaikovsky%2C_Pyotr)',
+  },
+  tchaikovskyNutcracker: {
+    imslp:
+      'https://imslp.org/wiki/The_Nutcracker_(ballet)%2C_Op.71_(Tchaikovsky%2C_Pyotr)',
+  },
+  tchaikovskySleepingBeauty: {
+    imslp:
+      'https://imslp.org/wiki/The_Sleeping_Beauty_(ballet)%2C_Op.66_(Tchaikovsky%2C_Pyotr)',
+  },
+  tchaikovskySwanLake: {
+    imslp:
+      'https://imslp.org/wiki/Swan_Lake_(ballet),_Op.20_(Tchaikovsky,_Pyotr)',
+  },
+  verdiForza: {
+    imslp: 'https://imslp.org/wiki/La_forza_del_destino_(Verdi%2C_Giuseppe)',
+  },
+  verdiNabucco: {
+    imslp: 'https://imslp.org/wiki/Nabucco_(Verdi%2C_Giuseppe)',
+  },
+  wagnerRheingold: {
+    imslp: 'https://imslp.org/wiki/Das_Rheingold,_WWV_86A_(Wagner,_Richard)',
+  },
+  wagnerFaust: {
+    imslp:
+      'https://imslp.org/wiki/Eine_Faust-Ouvert%C3%BCre%2C_WWV_59_(Wagner%2C_Richard)',
+  },
+  wagnerFlying: {
+    imslp:
+      'https://imslp.org/wiki/Der_fliegende_Holl%C3%A4nder%2C_WWV_63_(Wagner%2C_Richard)',
+  },
+  wagnerValkyrie: {
+    imslp: 'https://imslp.org/wiki/Die_Walk%C3%BCre,_WWV_86B_(Wagner,_Richard)',
+  },
+  wagnerGotterdammerung: {
+    imslp:
+      'https://imslp.org/wiki/G%C3%B6tterd%C3%A4mmerung,_WWV_86D_(Wagner,_Richard)',
+  },
+  wagnerLohengrin: {
+    imslp: 'https://imslp.org/wiki/Lohengrin,_WWV_75_(Wagner,_Richard)',
+  },
+  wagnerMeistersinger: {
+    imslp:
+      'https://imslp.org/wiki/Die_Meistersinger_von_N%C3%BCrnberg%2C_WWV_96_(Wagner%2C_Richard)',
+  },
+  wagnerParsifal: {
+    imslp: 'https://imslp.org/wiki/Parsifal,_WWV_111_(Wagner,_Richard)',
+  },
+  wagnerRienzi: {
+    imslp: 'https://imslp.org/wiki/Rienzi,_WWV_49_(Wagner,_Richard)',
+  },
+  wagnerSigfried: {
+    imslp: 'https://imslp.org/wiki/Siegfried,_WWV_86C_(Wagner,_Richard)',
+  },
+  wagnerTannhauser: {
+    imslp: 'https://imslp.org/wiki/Tannh%C3%A4user,_WWV_70_(Wagner,_Richard)',
+  },
+  weberFrieschutz: {
+    imslp:
+      'https://imslp.org/wiki/Der_Freisch%C3%BCtz%2C_Op.77_(Weber%2C_Carl_Maria_von)',
+  },
+  weberOberon: {
+    imslp: 'https://imslp.org/wiki/Oberon%2C_J.306_(Weber%2C_Carl_Maria_von)',
+  },
 };
 
 export default AllExcerptDataModel;
