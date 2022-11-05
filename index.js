e9bba4c847bce99dc94e18a95dbf5d1e29c0bbf2
Alexander Burdiss <aburdiss@icloud.com>
2022-11-05 17:11:10 -0400

    Remove app.json
    app.json is not required, and is only included because of historical reasons.
    see: react-native-community/cli#1113

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+const appName = 'BrassExcerpts';
 
 AppRegistry.registerComponent(appName, () => App);
