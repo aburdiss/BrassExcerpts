6a6978e88be437848cd1c64ad3fbad182e185ca3
Alexander Burdiss <aburdiss@icloud.com>
2023-08-22 15:17:00 -0400

    Updates React Native from 0.67.2 to 0.72.4 Updates Files according to: https://react-native-community.github.io/upgrade-helper/?from=0.67.2&to=0.72.4

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  semi: true,
+  tabWidth: 2,
+};
